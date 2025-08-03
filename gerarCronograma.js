24986568838466a734d5e0d120bf108934ef42ed
Arthur-8264 <arthurbenjaminalves3@gmail.com>
2025-08-02 23:02:23 -0300

    Delete gerarCronograma.js

--- gerarCronograma.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { gerarCronograma } = require('../controllers/gerarCronogramaController');
-
-router.post('/gerar-cronograma', gerarCronograma);
-
-module.exports = router;
-
